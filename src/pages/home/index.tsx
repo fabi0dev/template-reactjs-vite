b9940335026d4aa25de1a088ae1ed0d1e96791c6
Fábio Alves <fabioalves.to@gmail.com>
2024-03-01 17:04:01 -0300

    Adicionando alias

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,9 @@
+import { Header } from "../../components";
+
 export const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Header />
     </div>
   );
 };
